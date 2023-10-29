7f825ba08712119e60be1349cc389773e514508f
Santeri Aaltonen <89929476+AaltonenSan@users.noreply.github.com>
2023-10-29 15:08:40 +0200

    Changed docker-compose file and db creation scripts (#1)

--- server/src/db/scripts/create-db.ts
+++ server/src/db/scripts/create-db.ts
@@ -11,9 +11,21 @@ async function createDatabase() {
   knexConfig.connection.database = undefined;
 
   const knexInstance = knex(knexConfig);
-  await knexInstance.raw(`CREATE DATABASE ${dbName}`);
 
-  console.log("✅ Database created");
+  // Check if the database exists
+  const exists = await knexInstance
+    .select(knexInstance.raw("SCHEMA_NAME"))
+    .from("information_schema.SCHEMATA")
+    .where("SCHEMA_NAME", dbName);
+
+  if (exists.length === 0) {
+    // If database doesn't exist, create it
+    await knexInstance.raw(`CREATE DATABASE ${dbName}`);
+    console.log("✅ Database created");
+  } else {
+    console.log("❌ Database already exists");
+  }
+
   process.exit(0);
 }
 
--- server/src/db/scripts/migrate-db.ts
+++ server/src/db/scripts/migrate-db.ts
@@ -2,6 +2,8 @@ import getKnexInstance from "../knex";
 
 async function migrateDatabase() {
   const knexInstance = getKnexInstance();
+  // Drop tables before migrating data to reset db to initial state on every startup
+  await knexInstance.migrate.down();
   await knexInstance.migrate.up();
 
   console.log("✅ Database migrated");
